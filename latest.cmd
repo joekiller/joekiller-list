f2349caf2ec410a73afa84aa8c038c8c56b1c963
Joseph Lawson <joe@joekiller.com>
2024-03-10 00:58:11 -0500

    chore: update latest list

--- latest.cmd
+++ latest.cmd
@@ -6,3 +6,9 @@ mv playerlist-raw.joekiller.json.tmp playerlist-raw.joekiller.json
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
 mv "%userprofile%\Program Files\Pazer\cfg\playerlist.json" "%userprofile%\Program Files\Pazer\cfg\playerlist-%mydate%_%mytime%.json"
+for /F "tokens=1-3 delims=." %%a in ('git describe --tags --abbrev^=0') do (
+    set /a "b=%%b+1"
+    set /a version="%%a.%b%.0"
+    git commit -a -m "chore: update latest list"
+    git tag -a -m "%version%" %version%
+)
